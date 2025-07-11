ee07f62bf963c39a8400754774e7aace26a46964
ak-mishra03 <143195815+ak-mishra03@users.noreply.github.com>
2025-07-11 23:18:31 +0530

    added CRUD features and switced to class-based views cuz why not

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
 
-def post_list(request):
-    posts  = Post.objects.all()
-    return render(request,'home.html',{"posts":posts})
+class BlogListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "home.html"
 
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
 
-def post_detail(request,pk):
-    post= get_object_or_404(Post,pk=pk)
-    return render(request, "post-detail.html",{"post":post})
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author","body"]
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title","body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
